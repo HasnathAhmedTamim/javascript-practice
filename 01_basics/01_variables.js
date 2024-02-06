893dd8ac96e3b452de71d63e84c264e7410d01ae
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2024-02-06 13:18:08 +0600

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,31 @@
+const accountId = 111;
+let accountState;
+let accountEmail = "hasnath.tamim333@gmail.com";
+var accountPassword = "1235";
+
+accountCity = "Dhaka";
+
+// accountId = 2; not allowed
+
+accountEmail = "tamim@gmail.com";
+accountPassword = "000";
+
+// can declare like this but this not good
+accountCity = "Khilgoan";
+
+console.log(accountId);
+
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
+
+/*
+exploring javascript
+
+prefer not to use var because of issue in block scope and 
+functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,8 @@
+"use strict";
+// treat as js code as newer version
+
+// alert
+// alert(3 + 3); we are using nodeJs
+// code readability should high
+
+
