6a40e36e7a3b80311f4dce03fdb3973cb29a9ea5
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2024-02-06 14:00:46 +0600

    04_comparison

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// // auto convert 2 as number thats why true
+// console.log("012" > 1);
+// console.log("12" > "1");
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(typeof null);
+console.log(typeof 0)
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
